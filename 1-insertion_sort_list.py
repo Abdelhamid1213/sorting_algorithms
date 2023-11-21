65e9cf42ac00d3b82eea45343579fd6535510bc7
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-11-21 17:58:15 +0100

    Task 1

---
+++ 1-insertion_sort_list.py
@@ -0,0 +1,21 @@
+#!/bin/usr/python3
+
+def insertion_sort(array, size):
+    i = 1
+    tmp = array[i]
+    if array[i] < array[i-1]:
+        pass
+
+
+
+
+    return
+
+
+array = [19, 48, 99, 71, 13, 52, 96, 73, 86, 7]
+n = len(array)
+print(array)
+print('')
+insertion_sort(array, n)
+print('')
+print(array)
